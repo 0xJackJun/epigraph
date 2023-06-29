27f6c40a95a1922206631ee004c053a6e18989d2
jack <jackqr1996@gmail.com>
2023-06-29 14:37:18 +0800

    add deploy command

---
+++ README.md
@@ -0,0 +1,13 @@
+1. install foundry
+```
+curl -L https://foundry.paradigm.xyz | bash
+
+foundryup
+```
+2. run these commands
+```
+anvil
+
+forge script script/CEther.s.sol --rpc-url http://127.0.0.1:8545
+
+```
--- script/CEther.s.sol
+++ script/CEther.s.sol
@@ -2,11 +2,34 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Comptroller.sol";
+import "../src/CEther.sol";
+import { JumpRateModel } from "../src/JumpRateModel.sol";
+import { SimplePriceOracle, PriceOracle } from "../src/SimplePriceOracle.sol";
 
 contract CEtherScript is Script {
-    function setUp() public {}
+    Comptroller comptoller;
+    JumpRateModel jumpRateModel;
+    CEther cether;
+    SimplePriceOracle oracle;
+    function setUp() public {
+        uint baseRatePerYear = 20000000000000000;
+        uint multiplierPerYear = 200000000000000000;
+        uint jumpMultiplierPerYear = 2000000000000000000;
+        uint kink_ = 900000000000000000;
+        uint initialExchangeRateMantissa_ = 200000000000000000000000000;
+        uint8 decimals_ = 8;
+        comptoller = new Comptroller();
+        jumpRateModel = new JumpRateModel(baseRatePerYear, multiplierPerYear, jumpMultiplierPerYear, kink_);
+        cether = new CEther(comptoller, InterestRateModel(address(jumpRateModel)), initialExchangeRateMantissa_, "CEther", "cETH", decimals_, payable(address(this)));
+        oracle = new SimplePriceOracle();
+        oracle.setUnderlyingPrice(CToken(address(cether)), 1700);
+    }
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        vm.startBroadcast(deployerPrivateKey);
+        setUp();
+        vm.stopBroadcast();
     }
 }
